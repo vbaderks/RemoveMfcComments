ad6bddcc6a5e1c3b487c3a70f7be1eae3b8d65a1
Victor Derks <vbaderks@gmail.com>
2017-08-21 22:48:48 +0200

    Initial version

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveMfcComments
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            ReadAndClean("input.h", "output.h");
+        }
+
+        private static void ReadAndClean(string inputPath, string outputPath)
+        {
+            using (var inputFile = new StreamReader(inputPath, Encoding.GetEncoding("Windows-1252"), true))
+            using (var outputFile = new StreamWriter(outputPath, false, Encoding.UTF8))
+            {
+                string line;
+                while ((line = inputFile.ReadLine()) != null)
+                {
+                    if (IsLegacyMfcClassWizardComment(line))
+                        continue;
+                    outputFile.WriteLine(line);
+                }
+            }
+        }
+
+        private static bool IsLegacyMfcClassWizardComment(string line)
+        {
+            return line.Contains("//{{AFX_DATA_INIT") ||
+                   line.Contains("//}}AFX_DATA_INIT");
+        }
+    }
+}
