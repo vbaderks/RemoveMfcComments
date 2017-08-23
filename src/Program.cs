77cd203b66a44a3a11a774146eed8b00705b1b7b
Victor Derks <vbaderks@gmail.com>
2017-08-24 00:00:34 +0200

    Add more MFC comments

--- src/Program.cs
+++ src/Program.cs
@@ -27,8 +27,25 @@ namespace RemoveMfcComments
 
         private static bool IsLegacyMfcClassWizardComment(string line)
         {
-            return line.Contains("//{{AFX_DATA_INIT") ||
-                   line.Contains("//}}AFX_DATA_INIT");
+            line = line.Trim();
+
+            return line.Contains("//{{AFX_DATA(") ||
+                   line.Equals("//}}AFX_DATA") ||
+                   line.Contains("//{{AFX_DATA_INIT") ||
+                   line.Equals("//}}AFX_DATA_INIT") ||
+                   line.Contains("//{{AFX_DATA_MAP") ||
+                   line.Equals("//}}AFX_DATA_MAP") ||
+                   line.Contains("//{{AFX_MSG") ||
+                   line.Equals("//}}AFX_MSG") ||
+                   line.Contains("//{{AFX_MSG_MAP") ||
+                   line.Equals("//}}AFX_MSG_MAP") ||
+                   line.Equals("// Construction") ||
+                   line.Equals("// Dialog Data") ||
+                   line.Equals("// Overrides") ||
+                   line.Equals("// Implementation") ||
+                   line.Equals("// ClassWizard generated virtual function overrides") ||
+                   line.Equals("// NOTE: the ClassWizard will add message map macros here") ||
+                   line.Equals("// NOTE: the ClassWizard will add data members here");
         }
     }
 }
